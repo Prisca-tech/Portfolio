a32f51aac29a9cda14971e0c58f6c1c9d3d7fb89
Nyame-Wolf <mumenyam@gmail.com>
2022-06-13 23:37:49 +0300

    Remove logo and hamburger icon from view. Add font style and color

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const hamburger = document.getElementById("hamburger");
-const menu = document.getElementById("desktop-nav");
+const menu = document.getElementById("mobile-a-links");
+const logo = document.getElementById("logo");
 
 hamburger.addEventListener("click", () => {
   menu.classList.toggle("show");
+  logo.style.display = "none";
+  hamburger.style.display = "none";
 });
