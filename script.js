61fe895fa43138d300e07ad9f4ce80e68d713d9c
Nyame-Wolf <mumenyam@gmail.com>
2022-06-15 18:53:38 +0300

    Add javascript object dynamically to worksSection

--- script.js
+++ script.js
@@ -33,5 +33,29 @@ const workSection = [
   {
     sectionTitle: "My Recent works",
     image: { src: "./Image/Indicator.svg", alt: "an Indicator" },
+    articleTitle: "Multi-Post Stories Gain+Glory",
   },
 ];
+const sectionT = document.getElementById("works-intro");
+const artcle1Title = document.querySelector(".project-description");
+workSection.forEach((item) => {
+  const sectionH2 = document.createElement("h2");
+  const sectionH2Text = document.createTextNode(
+    `${workSection[0].sectionTitle}`
+  );
+  sectionH2.appendChild(sectionH2Text);
+  sectionT.appendChild(sectionH2);
+
+  const imgS = document.createElement("IMG");
+  imgS.setAttribute("src", `${workSection[0].image.src}`);
+  imgS.setAttribute("alt", `${workSection[0].image.alt}`);
+  document.getElementById("works-intro").appendChild(imgS);
+
+  const ArticleOneH4 = document.createElement("h4");
+  const ArticleOneH4Text = document.createTextNode(
+    `${workSection[0].articleTitle}`
+  );
+  console.log(workSection[0].articleTitle);
+  ArticleOneH4.appendChild(ArticleOneH4Text);
+  artcle1Title.appendChild(ArticleOneH4);
+});
