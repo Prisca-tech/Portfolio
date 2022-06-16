145aeecc2a1e55c96a81fe11b8dee1dc5079f89e
Nyame-Wolf <mumenyam@gmail.com>
2022-06-17 00:25:58 +0300

    Modify JS to work

--- script.js
+++ script.js
@@ -1,46 +1,44 @@
-const hamburger = document.getElementById('hamburger');
-const menu = document.getElementById('desktop-nav');
-const logo = document.getElementById('logo');
-const icon = document.getElementById('icon');
-const about = document.getElementById('about');
-const contact = document.getElementById('contact');
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("desktop-nav");
+const logo = document.getElementById("logo");
+const icon = document.getElementById("icon");
+const about = document.getElementById("about");
+const contact = document.getElementById("contact");
 
-hamburger.addEventListener('click', () => {
-  menu.classList.toggle('show');
-  logo.style.display = 'none';
-  hamburger.style.display = 'none';
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("show");
+  logo.style.display = "none";
+  hamburger.style.display = "none";
 });
 
-icon.addEventListener('click', () => {
-  menu.classList.remove('show');
-  logo.style.display = 'block';
-  hamburger.style.display = 'block';
+icon.addEventListener("click", () => {
+  menu.classList.remove("show");
+  logo.style.display = "block";
+  hamburger.style.display = "block";
 });
 
-about.addEventListener('click', () => {
-  menu.classList.remove('show');
-  logo.style.display = 'block';
-  hamburger.style.display = 'block';
+about.addEventListener("click", () => {
+  menu.classList.remove("show");
+  logo.style.display = "block";
+  hamburger.style.display = "block";
 });
 
-contact.addEventListener('click', () => {
-  menu.classList.remove('show');
-  logo.style.display = 'block';
-  hamburger.style.display = 'block';
+contact.addEventListener("click", () => {
+  menu.classList.remove("show");
+  logo.style.display = "block";
+  hamburger.style.display = "block";
 });
-
-const form = document.getElementById("personal-info");
-const target = document.getElementById('form-valid')
 
 const myRegex = /[A-Z]/;
-form.addEventListener("submit", function (event) {
-  const email = form.elements["email"];
-  const errorMessage = ''
+const form = document.getElementById("personal-info");
+form.addEventListener("submit", (event) => {
+  const { email } = form.elements;
   const emailAddress = email.value;
-if(myRegex.test(emailAddress === true)){
-  event.preventDefault();
-  errorMessage = "Please ensure that email is in lowercase letters"
-  target.innerHTML = errorMessage
-}
-  
+  const errM = document.querySelector("#form-valid");
+  if (myRegex.test(emailAddress) === true) {
+    event.preventDefault();
+    const message =
+      "Email field should only have <strong>lowercase</strong> letters";
+    errM.innerHTML = message;
+  }
 });
