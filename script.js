fc4158d2ffd65cfff5a9ecb28ba7f00d30bc34c6
Priscilla Okoro <oyomepriscilla@gmail.com>
2022-06-22 20:15:18 -0600

    add form validation to email

--- script.js
+++ script.js
@@ -28,3 +28,16 @@ contact.addEventListener('click', () => {
   logo.style.display = 'block';
   hamburger.style.display = 'block';
 });
+
+const myRegex = /[A-Z]/;
+const form = document.getElementById('personal-info');
+form.addEventListener('submit', (event) => {
+  const { email } = form.elements;
+  const emailAddress = email.value;
+  const errM = document.querySelector('#form-valid');
+  if (myRegex.test(emailAddress) === true) {
+    event.preventDefault();
+    const message = 'Please ensure that all email input is in <strong> Lower case </strong>';
+    errM.innerHTML = message;
+  }
+});
